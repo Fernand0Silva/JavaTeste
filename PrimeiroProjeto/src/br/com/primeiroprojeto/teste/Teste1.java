1c8814d5a6735f2df87871a451efbe16eb542301
Luiz <luizfernando3995@gmail.com>
2024-08-03 19:25:35 -0300

    primeiro commit

---
+++ PrimeiroProjeto/src/br/com/primeiroprojeto/teste/Teste1.java
@@ -0,0 +1,21 @@
+package br.com.primeiroprojeto.teste;
+
+public class Teste1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello ");
+
+		String nome = "Luiz";
+		System.out.println("Bem - Vindo,"+ nome);
+		soma();
+	}
+	static void soma() {
+		int n = 10;
+		int n1 = 20;
+		int r = n+n1;
+		System.out.println(r);
+
+	}
+	
+}
